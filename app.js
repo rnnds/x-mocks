6bbe3984773129581160179ce76b6576bba6d865
Ronan Silva <ronan.ads@gmail.com>
2016-01-25 20:07:57 -0200

    Extracting scanner component

--- app.js
+++ app.js
@@ -1,30 +1,12 @@
 var koa = require('koa'),
   route = require('koa-route'),
   app = module.exports = koa(),
-  fs = require('fs'),
-  mappedRoutes = {};
+  scanner = require('./scanner.js')(app);
 
-app.use(route.get('/routes', routes));
+app.use(route.get('/routes', function*() {
+  this.body = JSON.stringify(scanner.routes);
+}));
 
-function* routes() {
-  this.body = JSON.stringify(mappedRoutes);
-}
-
-function* show(data) {
-  var mappedRoute = mappedRoutes[this.url];
-  this.status = mappedRoute.status;
-  this.body = mappedRoute.body;
-}
-
-fs.readdir('mocks', function(err, items) {
-  for (var i = 0; i < items.length; i++) {
-    var item = items[i];
-    fs.readFile('mocks/' + item, 'utf8', function(err, data) {
-      var obj = JSON.parse(data);
-      mappedRoutes[obj.uri] = obj.response;
-      app.use(route[obj.method](obj.uri, show));
-    });
-  }
-});
+scanner.scan();
 
 if (!module.parent) app.listen(3000);
---
+++ scanner.js
@@ -0,0 +1,32 @@
+var fs = require('fs'),
+  route = require('koa-route'),
+  _ = require('lodash-node'),
+  mappedRoutes = {};
+
+function scan() {
+  var items = fs.readdirSync('mocks');
+  _.forEach(items, function(item) {
+    var obj = JSON.parse(fs.readFileSync('mocks/' + item));
+    mappedRoutes[identifier(obj.method, obj.uri)] = obj.response;
+    app.use(route[obj.method](obj.uri, show));
+    console.info('Registered route: %s', item);
+  });
+};
+
+function identifier(method, uri) {
+  return method.toLowerCase() + ':' + uri;
+};
+
+function* show() {
+  var mappedRoute = mappedRoutes[identifier(this.request.method, this.url)];
+  this.status = mappedRoute.status;
+  this.body = mappedRoute.body;
+};
+
+module.exports = function(app) {
+  this.app = app;
+  return {
+    scan: scan,
+    routes: mappedRoutes,
+  }
+}
